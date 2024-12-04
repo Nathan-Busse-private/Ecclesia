dfbb54b7712806744617bb23783a7bfed9be0f9a
Nathan Busse <bussenathan@icloud.com>
2024-12-04 10:21:42 +0200

    Modified Gradle

--- src/app/ecclesia/build.gradle.kts
+++ src/app/ecclesia/build.gradle.kts
@@ -48,6 +48,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("com.google.android.material:material:1.12.0")
     androidTestImplementation(platform("androidx.compose:compose-bom:2024.04.01"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
