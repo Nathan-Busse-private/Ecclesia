2ac4ccd7853be2e3c4ac979ba551aa6c45cf76ef
Nathan Busse <bussenathan@icloud.com>
2024-12-04 07:29:07 +0200

    Project creation in Android Studios
    A seed into a tree.

---
+++ src/app/build.gradle.kts
@@ -0,0 +1,40 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+}
+
+android {
+    namespace = "com.nathanimus.ecclesia"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.nathanimus.ecclesia"
+        minSdk = 28
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+}
