9387a6bc1c9c74ca0c9c7c8b178ab2eb10ec33c8
Nathan Busse <bussenathan@icloud.com>
2024-12-04 09:50:25 +0200

    Changed string name

--- src/app/ecclesia/src/main/java/com/nathanimus/ecclesia/MainActivity.kt
+++ src/app/ecclesia/src/main/java/com/nathanimus/ecclesia/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             EcclesiaTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Ecclesia",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -42,6 +42,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     EcclesiaTheme {
-        Greeting("Android")
+        Greeting("Ecclesia")
     }
 }
