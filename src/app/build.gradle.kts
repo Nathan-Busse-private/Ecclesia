eb20b2702f8af7b144aea39c55d484d546888e27
Nathan Busse <bussenathan@icloud.com>
2024-12-05 08:56:50 +0200

    Upgraded target SDK from 34 to 35

--- src/app/build.gradle.kts
+++ src/app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.nathanimus.ecclesia"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.nathanimus.ecclesia"
         minSdk = 28
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
